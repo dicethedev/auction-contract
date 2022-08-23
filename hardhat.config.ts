cf8f7d8aa99c8bc39a1562aa8ba0e82673d8405b
dicetheDev <samuelblessing125@gmail.com>
2022-08-23 12:14:37 +0100

    intial commit

---
+++ hardhat.config.ts
@@ -0,0 +1,24 @@
+import { HardhatUserConfig } from "hardhat/config";
+import "@nomicfoundation/hardhat-toolbox";
+//this will help you to verify your address on Etherscan
+import "@nomiclabs/hardhat-etherscan";
+
+const dotenv = require('dotenv');
+dotenv.config();
+
+/** @type import('hardhat/config').HardhatUserConfig */
+//@dev - pass the network properties
+
+const config: HardhatUserConfig = {
+   solidity: "0.8.9",
+   networks: {
+    goerli: {
+      url: process.env.GOERLI_RPC_URL,
+      accounts: process.env.PRIVATE_KEY !== undefined ? [process.env.PRIVATE_KEY] : [],
+    },
+  },
+  etherscan: {
+    apiKey: process.env.ETHERSCAN_KEY,
+  },
+};
+export default config;
---
+++ scripts/deploy.ts
@@ -0,0 +1,25 @@
+import { ethers } from "hardhat";
+
+async function main() {
+
+ let [bidder1, bidder2] = await ethers.getSigners();
+
+const Auction = await ethers.getContractFactory("Auction");
+const auction = await Auction.deploy();
+
+await auction.deployed();
+
+//this will send one ether along when deploying
+// const bidAmount = ethers.utils.parseEther("0.010");
+
+console.log("Auction contract deployed to this address: ", auction.address);
+
+const createAuction = await auction.auctionAction( 10, bidder2.address);
+
+}
+// We recommend this pattern to be able to use async/await everywhere
+// and properly handle errors.
+main().catch((error) => {
+console.error(error);
+process.exitCode = 1;
+});
